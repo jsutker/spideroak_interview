83067cd028511029d6204d0c6a95193ccabecd6a
Joseph Sutker <jomasu22@gmail.com>
2023-05-15 00:37:44 -0400

    Fill out encrypt_to_file

--- lib/ssl_wrapper.c
+++ lib/ssl_wrapper.c
@@ -6,6 +6,7 @@
 #include <ssl_wrapper.h>
 
 static unsigned char salt[] = {'s','p','i','d','e','r','o','a','k'};
+static unsigned char salt2[] = {'a','e','s','2','5','6','c','b','c'};
 
 int encrypt_to_file(char* pass, char* message, char* filepath)
 {
@@ -13,7 +14,7 @@ int encrypt_to_file(char* pass, char* message, char* filepath)
    * Basic encryption of a message using a passphrase
    * @param pass     The passphrase
    * @param message  The message to encrypt
-   * @param filepath A pointer where we are passing back our encrypted message
+   * @param filepath The path of a file to which we are writing our encrypted message
    * @return         1 if successful, 0 if not
    */
   
@@ -23,7 +24,24 @@ int encrypt_to_file(char* pass, char* message, char* filepath)
   {
     return 0;
   }
-  
-  // TODO: Actually encrypt/write file, above is just generating key from passphrase
 
+  unsigned char iv[32] = {};
+  if (0 == PKCS5_PBKDF2_HMAC_SHA1(pass, strlen(pass), salt2, 9, 1, 32, iv))
+  {
+    return 0;
+  }
+  
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  EVP_CipherInit(ctx, EVP_aes_256_cbc(), key, iv, 1);
+  unsigned int blocksize = EVP_CIPHER_CTX_block_size(ctx);
+  unsigned char *buf = malloc((sizeof(char)*strlen(message))+blocksize);
+  int encrypted_len;
+  EVP_CipherUpdate(ctx, buf, &encrypted_len, message, strlen(message));
+
+  FILE *fp = fopen(filepath, "wb");
+  fwrite(buf, sizeof(unsigned char), encrypted_len, fp);
+  fclose(fp);
+
+  EVP_CIPHER_CTX_free(ctx);
+  return 1;
 }
